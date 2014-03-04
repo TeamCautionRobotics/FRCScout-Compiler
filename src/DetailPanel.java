da050221f886ed594826eb8509e5259a1a154fb8
docprofsky <schuyler.st.leger@gmail.com>
2014-03-04 10:02:32 -0700

    Fixed formatting and imports

--- src/DetailPanel.java
+++ src/DetailPanel.java
@@ -1,20 +1,18 @@
-import java.awt.Component;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
 public class DetailPanel extends JPanel {
-	
+
 	private static final long serialVersionUID = 1542798734160584916L;
-	
+
 	private JLabel titleLabel;
-	
+
 	public JLabel queryLabel;
 
 	public DetailPanel(MainFrame mainFrame) {
 		super();
-		titleLabel = new JLabel("Title Here");add(titleLabel);
+		titleLabel = new JLabel("Team Number:");
+		add(titleLabel);
 		queryLabel = new JLabel();
 		add(queryLabel);
 	}
--- src/ResultList.java
+++ src/ResultList.java
@@ -2,14 +2,11 @@ import javax.swing.JList;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
-import sun.rmi.runtime.Log;
-
-
 public class ResultList extends JList implements ListSelectionListener {
-	
+
 	private static final long serialVersionUID = 2793884482054862476L;
-	
-	private String[] resultDataList = new String[]{"Autonmous", "Shoot", "Working"};
+
+	private String[] resultDataList = new String[] {"1492", "842", "3617"};
 
 	public ResultList(MainFrame mainFrame) {
 		super();
@@ -20,7 +17,8 @@ public class ResultList extends JList implements ListSelectionListener {
 	@Override
 	public void valueChanged(ListSelectionEvent e) {
 		System.out.println(this.getSelectedIndex());
-		MainFrame.detailPanel.queryLabel.setText(resultDataList[this.getSelectedIndex()]);
+		MainFrame.detailPanel.queryLabel.setText(resultDataList[this
+				.getSelectedIndex()]);
 	}
-	
+
 }
